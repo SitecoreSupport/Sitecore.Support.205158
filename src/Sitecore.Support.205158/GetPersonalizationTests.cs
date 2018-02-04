18855cf13a7abba555f9d2e50739190d406144e5
Ivan Sheyenko <ivs@sitecore.net>
2018-02-04 11:47:26 +0200

    Original code

---
+++ src/Sitecore.Support.205158/GetPersonalizationTests.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.ContentTesting.Inspectors;
+using Sitecore.ContentTesting.Pipelines.GetTests;
+using Sitecore.Diagnostics;
+using Sitecore.Layouts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.ContentTesting.Tests;
+
+namespace Sitecore.Support.ContentTesting.Pipelines.GetTests
+{
+  public class GetPersonalizationTests : GetTestsProcessor
+  {
+    public override void Process(GetTestsArgs args)
+    {
+      Assert.ArgumentNotNull(args.Item, "args.Item");
+      foreach (RenderingDefinition current in new TestingRenderingInspector(args.Item, null)
+      {
+        InspectionMode = TestingRenderingInspector.Mode.Personalization,
+        DeviceId = args.DeviceId,
+        EnsureTestIsNotRunning = true
+      }.GetRenderings())
+      {
+        args.AddTest(new PersonalizationTest(current, args.Item));
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.205158/PersonalizationTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Sitecore.ContentTesting.Tests;
+using Sitecore.Data.Items;
+using Sitecore.Layouts;
+using Sitecore.ContentTesting.Model.Data.Items;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Diagnostics;
+using Sitecore.Xml;
+
+namespace Sitecore.Support.ContentTesting.Tests
+{
+  public class PersonalizationTest : Sitecore.ContentTesting.Tests.PersonalizationTest, ITest
+  {
+    public PersonalizationTest(RenderingDefinition renderingDefinition, Item contentItem) : base(renderingDefinition, contentItem)
+    {
+    }  
+
+    bool ITest.Remove(bool deleteItems)
+    {
+      TestDefinitionItem testDefinitionItem = this.GetTestDefinitionItem(this.renderingDefinition, this.contentItem.Database);
+      this.RemoveInField(FieldIDs.LayoutField);
+      this.RemoveInField(FieldIDs.FinalLayoutField);
+      if (testDefinitionItem != null)
+      {
+        if (deleteItems)
+        {
+          testDefinitionItem.InnerItem.Delete();
+        }
+        else
+        {
+          testDefinitionItem.InnerItem.Recycle();
+        }
+      }
+      return true;
+    }    
+  }
+}
